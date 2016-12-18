39a4dac389a65519e5400c98072afcb0cf3ebd89
Semen Kamenetskiy <skamenetskiy@live.com>
2016-12-18 23:42:10 +0300

    docs

--- gruntfile.js
+++ gruntfile.js
@@ -54,17 +54,44 @@ module.exports = (grunt) => {
                 src:     'src/**/*.html',
                 dest:    'dist/bulma.js'
             }
+        },
+        copy:        {
+            docs_js:  {
+                src:     [
+                    'bower_components/angular/angular.min.js',
+                    'dist/bulma.min.js'
+                ],
+                dest:    'docs/js/',
+                filter:  'isFile',
+                expand:  true,
+                flatten: true
+            },
+            docs_css: {
+                src:     [
+                    'bower_components/bulma/css/bulma.css'
+                ],
+                dest:    'docs/css/',
+                filter:  'isFile',
+                expand:  true,
+                flatten: true
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-angular-templates');
 
     grunt.registerTask('default', [
         'concat:dist',
         'ngtemplates:dist',
         'uglify:dist'
+    ]);
+
+    grunt.registerTask('docs', [
+        'copy:docs_js',
+        'copy:docs_css'
     ]);
 
     function ngTemplatesWrapper(module, script) {
